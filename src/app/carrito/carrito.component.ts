5f1e069c88d6b1ce1eed2a4793d5ba95d9b9498b
Ignacio Montenegro <chupsris@gmail.com>
2022-10-12 21:28:58 -0300

    Carrito Agregado!

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../app/services/item.service'
 import { Item } from '../../app/interfaces/item';
+import { CarroService } from '../services/carro.service';
 
 @Component({
   selector: 'app-carrito',
@@ -10,11 +11,19 @@ import { Item } from '../../app/interfaces/item';
 export class CarritoComponent implements OnInit {
 
   listaItems: Item[] = [];
+  public listaProductos : any;
+  public productos : any;
+  public totalfinal !: number;
 
-  constructor(private _itemService: ItemService) { }
+  constructor(private _itemService: ItemService, private _carroService: CarroService) { }
 
   ngOnInit(): void {
     this.obtenerItems();
+    this._carroService.obtenerProductos()
+    .subscribe(res=>{
+      this.productos = res;
+      this.totalfinal = this._carroService.obtenerPrecio();
+    })
   }
 
   obtenerItems() {
@@ -25,4 +34,9 @@ export class CarritoComponent implements OnInit {
       console.log(error);
     })
   }
+
+  quitarItem(item:any){
+    this._carroService.quitarItemCarro(item);
+  }
+
 }
--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../app/services/item.service'
 import { Item } from '../../app/interfaces/item';
+import { CarroService } from '../services/carro.service';
 
 @Component({
   selector: 'app-cliente',
@@ -11,7 +12,7 @@ export class ClienteComponent implements OnInit {
 
   listaItems: Item[] = [];
 
-  constructor(private _itemService: ItemService) { }
+  constructor(private _itemService: ItemService, private _carroService: CarroService) { }
 
   ngOnInit(): void {
     this.obtenerItems();
@@ -24,6 +25,11 @@ export class ClienteComponent implements OnInit {
     }, error => {
       console.log(error);
     })
+  }
+
+  agregarCarro(item : any) {
+    this._carroService.agregarCarro(item);
+    
   }
 
 }
