0a4850d5157eae0691b113c90748e01c5ecd0c4f
Luis Gonzalez <lugori97@gmail.com>
2022-11-17 23:35:56 -0300

    carta emplado

--- src/app/empleado/menu-empleado/menu-empleado.component.ts
+++ src/app/empleado/menu-empleado/menu-empleado.component.ts
@@ -1,4 +1,17 @@
+import { Categoria } from '../../interfaces/categoria';
+import { CategoriasService } from '../../services/categorias.service';
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from 'src/app/services/item.service';
+import { Item } from 'src/app/interfaces/item';
+import { CarroService } from 'src/app/services/carro.service';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Router } from "@angular/router"
+import { Carro } from 'src/app/interfaces/carro';
+import { CookieService } from 'ngx-cookie-service';
+import { Inject } from '@angular/core';
+
 
 @Component({
   selector: 'app-menu-empleado',
@@ -6,10 +19,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-empleado.component.scss']
 })
 export class MenuEmpleadoComponent implements OnInit {
+  listaItems: any[] = [];
+  cat: Categoria[] = [];
+  codeTable: String;
+  mesaId: Number;
+  carro: Carro;
+  public cookieValue: any[] = [];
+  indexCantidad: number[] = [];
+  public totalItems: number = 0;
 
-  constructor() { }
+  constructor(private _itemService: ItemService, private _categoriaService: CategoriasService, private toastr: ToastrService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.cargarCategorias();
+    this.obtenerItems();
+
+  }
+
+  cargarCategorias() {
+    this._categoriaService.obtenerCategorias().subscribe(data => {
+      this.cat = data;
+      this.cat.forEach(categoria => {
+        categoria.subcategoria.sort();
+      });
+    })
+  }
+
+  obtenerItems() {
+    this._itemService.getItems().subscribe(data => {
+      for(let i = 0; i<this.cat.length;i++){
+        data.forEach(element => {
+          if(element.categoria==this.cat[i].categoria)
+          this.listaItems.push(element);
+        });
+      }
+
+    }, error => {
+      console.log(error);
+    })
   }
 
 }
