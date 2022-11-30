aba8a04bcef4e29254bc3c130926a46460daa350
Ignacio Montenegro <chupsris@gmail.com>
2022-11-30 20:48:33 -0300

    Categorias Extraidas

--- src/app/admin/vista-categorias/vista-categorias.component.ts
+++ src/app/admin/vista-categorias/vista-categorias.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CategoriasService } from 'src/app/services/categorias.service';
+import { ModalItemComponent } from '../modal-item/modal-item.component';
 
 @Component({
   selector: 'app-vista-categorias',
@@ -7,9 +10,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaCategoriasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _categoriaService: CategoriasService, private toastr: ToastrService) {
+    this._categoriaService.Refreshrequired.subscribe(result => {
+      this.obtenerCategorias();
+    })
+    this._categoriaService.Refreshrequired.subscribe(result => {
+      this.obtenerSubCategorias();
+    })
+   }
 
   ngOnInit(): void {
+    this.obtenerCategorias();
+    this.obtenerSubCategorias();
+    console.log(this.listaCategorias,this.listaSubcategorias)
   }
+
+  listaCategorias: any[] = [];
+  listaSubcategorias: any[] = [];
+  formModal: any;
+
+  @ViewChild(ModalItemComponent) addView !: ModalItemComponent
+
+  obtenerCategorias() {
+    this._categoriaService.obtenerCategorias().subscribe(data => {
+      this.listaCategorias = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+  obtenerSubCategorias() {
+    this._categoriaService.obtenerSubCategorias().subscribe(data => {
+      this.listaSubcategorias = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  eliminarCategoria(id: any) {
+    if (confirm('Esta seguro de eliminar?'))
+      this._categoriaService.eliminarCategoria(id).subscribe(data => {
+        this.toastr.error('La categoria fue eliminada con exito', 'Categoria eliminada');
+        this.obtenerCategorias();
+      }, error => {
+        console.log(error);
+      })
+  }
+
+  editarCategoria(id: any) {
+    this.addView.cargarCategoriaEditar(id);
+  }
+
+  cambiarEstado($event, categoriaid, evento){
+      if (!confirm('Esta seguro?')) {
+        $event.stopPropagation();
+        return false;
+      }
+      else {
+        this._categoriaService.obtenerCategorias().subscribe(res => {
+          res.estado = 'eliminada';
+
+          this._categoriaService.actualizarCategoria(res, categoriaid).subscribe(data => {
+            this.toastr.success('Categoria eliminada con exito');
+          }, error => {
+            console.log(error);
+          });
+        })
+        return true;
+      }
+      return false;
+  }
+
+  doSomething() {
+    this.obtenerCategorias();
+  }
+
 
 }
--- src/app/services/categorias.service.ts
+++ src/app/services/categorias.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Categoria } from '../interfaces/categoria';
 
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
+  private _refreshrequired = new Subject<void>();
+
+  get Refreshrequired(){
+    return this._refreshrequired;
+  }
   url = 'https://produccion2.herokuapp.com/api/categoria/';
 
   constructor(private http: HttpClient) { }
@@ -18,5 +23,21 @@ export class CategoriasService {
 
   obtenerCategoriasTotal(): Observable<any> {
     return this.http.get(this.url + 'categorias');
+  }
+
+  eliminarCategoria(id: String): Observable<any> {
+    return this.http.delete(this.url + id);
+  }
+
+  obtenerSubCategorias(): Observable<any> {
+    return this.http.get(this.url + 'subcategorias');
+  }
+
+  actualizarCategoria(categoria:Categoria, id:string): Observable<any> {
+    return this.http.put(this.url+id, categoria).pipe(
+      tap(() => {
+        this._refreshrequired.next();
+      })
+    );
   }
 }
